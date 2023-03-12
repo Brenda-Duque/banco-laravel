b0b0b98e4a8e03c03df6f23620d6741ea4a6409b
Brenda Duque <brendabduque@hotmail.com>
2023-03-12 16:56:58 -0300

    Teste unitário

---
+++ tests/Unit/UserRegisterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Account;
+use App\Models\Lojista;
+use App\Models\User;
+
+class UserRegisterTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_columns_user_common() {
+
+        $user = new User();
+
+        $expected = [
+            'type',
+            'name',
+            'email',
+            'cpf_cnpj',
+            'password'
+        ];
+        $arrayCompared = array_diff($expected, $user->getFillable());
+
+        $this->assertEquals(0, count($arrayCompared));
+    }
+
+    public function test_column_shopkeeper() {
+        $user = new User();
+        $lojista = new LOjista();
+
+        $expected = [
+            'type',
+            'name',
+            'email',
+            'cpf_cnpj',
+            'password'
+        ];
+
+        $expected_ = [
+            'company_name'
+        ];
+
+        $arrayCompared = array_diff($expected, $user->getFillable());
+        $arrayCompared_ = array_diff($expected_, $lojista->getFillable());
+
+        $this->assertEquals(0, count($arrayCompared));   
+        $this->assertEquals(0, count($arrayCompared_));     
+    }
+
+    public function test_register_user_common() {
+
+        $user = new User();
+
+        $attributes = [
+            'type'     => 'common',
+            'name'     => 'Brenda Duque',
+            'email'    => 'brendaduqueee@gmail.com',
+            'cpf_cnpj' => '38530525094',
+            'password' => 'Areia@2023',
+        ];
+        User::create($attributes);
+
+        $this->assertDatabaseHas('users', [
+            'type'     => 'common',
+            'name'     => 'Brenda Duque',
+            'email'    => 'brendaduqueee@gmail.com',
+            'cpf_cnpj' => '38530525094',
+            'password' => 'Areia@2023',
+        ]);
+
+        $this->assertTrue(true);
+    }
+}
